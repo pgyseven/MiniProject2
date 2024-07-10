7af2ce4f96d6996a61a8580bce9a68774e779d10
webshjin <web.shjin@gmail.com>
2024-07-10 12:05:41 +0900

    feat : 게시판 전체목록 출력 기능을 위한 Service단 구현

---
+++ src/main/java/com/miniproj/service/hboard/HBoardService.java
@@ -0,0 +1,26 @@
+package com.miniproj.service.hboard;
+
+import java.util.List;
+
+import com.miniproj.model.HBoardVO;
+
+
+public interface HBoardService {
+	
+	// 게시판 전제 리스트 조회
+	public List<HBoardVO> getAllBoard();
+	
+	// 게시판 글 작성
+	
+	
+	// 게시판 글 수정
+	
+	
+	// 게시판 상세 보기
+	
+	
+	// 게시판 글 삭제
+	
+	
+	
+}
---
+++ src/main/java/com/miniproj/service/hboard/HBoardServiceImpl.java
@@ -0,0 +1,30 @@
+package com.miniproj.service.hboard;
+
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import com.miniproj.model.HBoardVO;
+import com.miniproj.persistence.HBoardDAO;
+
+@Service   // 아래의 클래스가 서비스 객체임을 컴파일러에 공지
+public class HBoardServiceImpl implements HBoardService {
+	
+	@Autowired
+	private HBoardDAO bDao;
+	
+	@Override
+	public List<HBoardVO> getAllBoard() {
+		
+		System.out.println("HBoardServiceImpl.....");
+		
+		// DAO 단 호출
+		List<HBoardVO> lst = bDao.selectAllBoard();
+		
+		return lst;
+	}
+
+}
