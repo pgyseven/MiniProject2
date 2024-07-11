330688a96864e100310e802f46497637066fae1f
webshjin <web.shjin@gmail.com>
2024-07-11 16:51:47 +0900

    feat : 게시글을 저장하는 메소드

--- src/main/java/com/miniproj/persistence/HBoardDAO.java
+++ src/main/java/com/miniproj/persistence/HBoardDAO.java
@@ -2,10 +2,14 @@ package com.miniproj.persistence;
 
 import java.util.List;
 
+import com.miniproj.model.HBoardDTO;
 import com.miniproj.model.HBoardVO;
 
 public interface HBoardDAO {
 
 	// 게시판의 전체리스트를 가져오는 메서드
 	List<HBoardVO> selectAllBoard() throws Exception;  // 몸체가 없는 추상메서드
+	
+	// 게시글을 저장하는 메소드
+	int insertNewBoard(HBoardDTO newBoard) throws Exception;
 }
--- src/main/java/com/miniproj/persistence/HBoardDAOImpl.java
+++ src/main/java/com/miniproj/persistence/HBoardDAOImpl.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.miniproj.model.HBoardDTO;
 import com.miniproj.model.HBoardVO;
 
 @Repository  // 아래의 클래스가 DAO객체임을 명시
@@ -29,6 +30,12 @@ public class HBoardDAOImpl implements HBoardDAO {
 //		}
 		
 		return list; 
+	}
+
+	@Override
+	public int insertNewBoard(HBoardDTO newBoard) throws Exception {
+		
+		return ses.insert(NS + ".saveNewBoard", newBoard);
 	}
 
 }
