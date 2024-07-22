024d417d7f6921424adbad1da45b6dba8bcab817
webshjin <web.shjin@gmail.com>
2024-07-22 10:49:49 +0900

    feat : 조인문의 결과를 담기 위한 VO객체 생성

---
+++ src/main/java/com/miniproj/model/BoardDetailInfo.java
@@ -0,0 +1,33 @@
+package com.miniproj.model;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class BoardDetailInfo {
+	private int boardNo;
+	private String title;
+	private String content;
+	private String writer;
+	private Timestamp postDate;
+	private int readCount;
+	
+	private List<BoardUpFilesVODTO> fileList;
+	
+	private String userName;
+	private String email;
+	
+}
