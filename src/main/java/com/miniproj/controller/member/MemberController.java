61157bca4ad4314729ba2de86ff02ccd06b5ba1f
gayoon-create <kgykgy1123@naver.com>
2024-08-01 12:04:39 +0900

    feat : requestparam에 userProfile 넣어줬음

--- src/main/java/com/miniproj/controller/member/MemberController.java
+++ src/main/java/com/miniproj/controller/member/MemberController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.miniproj.model.MemberVO;
 import com.miniproj.model.MyResponseWithoutData;
@@ -26,9 +27,9 @@ public class MemberController {
       
    }
    @RequestMapping(value="/register", method = RequestMethod.POST)
-   public void registerMember(MemberVO registMember) {
+   public void registerMember(MemberVO registMember, @RequestParam("userProfile") MultipartFile userProfile) {
       System.out.println("회원가입 진행~~~~~~~~~~~~~~~~" + registMember.toString());
-      
+      System.out.println(userProfile.getOriginalFilename());
    }
    
    
