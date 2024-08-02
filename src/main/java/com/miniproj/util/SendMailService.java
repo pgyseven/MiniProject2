5178f1333e3cbdcf90ccacb679ddc698c1a44f11
gayoon-create <kgykgy1123@naver.com>
2024-08-02 17:48:07 +0900

    fix : 계정 정보를 properties 파일에서 읽어오도록 했다.(계정 정보가 깃허브에 노출되지 않도록)

--- src/main/java/com/miniproj/util/SendMailService.java
+++ src/main/java/com/miniproj/util/SendMailService.java
@@ -1,5 +1,7 @@
 package com.miniproj.util;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -12,11 +14,19 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
 public class SendMailService {
-	private String username = "kgykgy1123";
-	private String password ="WFSVXGX5TXPW";
 	
-	public void sendMail(String emailAddr, String activationCode) throws AddressException, MessagingException {
+	@Autowired
+	ResourceLoader resourceLoader;
+	
+	private String username;
+	private String password;
+	
+	public void sendMail(String emailAddr, String activationCode) throws AddressException, MessagingException, IOException {
 		String subject = "miniproject.com에서 보내는 회원가입 이메일 인증번호 입니다.";
 		String message = "회원가입을 환영합니다. 인증번호 : " + activationCode + "를 입력하시고 회원가입을 완료해주세요.";
 		
@@ -31,6 +41,10 @@ public class SendMailService {
 		props.put("mail.smtp.ssl.protocols", "TLSv1.2"); // 보안인증을 위한 설정
 		props.put("mail.smtp.ssl.enable", "true");  // SSL 사용
 		props.put("mail.smtp.auth", "true"); // 인증 과정을 거치겠다.
+		
+		
+		
+		getAccount();
 		
 		Session mailSession = Session.getInstance(props, new Authenticator() {
 
@@ -60,5 +74,15 @@ public class SendMailService {
 			trans.send(mime);
 			trans.close();
 		}
+	}
+
+	private void getAccount() throws IOException {
+		
+		Properties account = new Properties();
+
+		account.load(new FileReader("D:\\lecture\\spring\\MiniProject\\src\\main\\webapp\\WEB-INF\\config.properties"));
+		
+		this.username = (String)account.get("username");
+		this.password = (String)account.get("password");
 	}
 }
