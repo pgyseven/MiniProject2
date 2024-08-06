edca8c26c32faa85aa778c3e1b39f7cf5d282cb2
gayoon-create <kgykgy1123@naver.com>
2024-08-06 14:14:05 +0900

    feat : 서비스단 다녀온 후 로그인 성공시 로그인을 한 유저의 정보를 model 객체에 넣어줌(인터셉터로 넘겨서 나머지 로그인 처리를 하기 위해서)

--- src/main/java/com/miniproj/controller/member/MemberController.java
+++ src/main/java/com/miniproj/controller/member/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -176,7 +177,26 @@ public class MemberController {
    }
    
    @RequestMapping(value="/login", method=RequestMethod.POST)
-   public void loginPOST(LoginDTO loginDTO) {
+   public void loginPOST(LoginDTO loginDTO, Model model) {
 	   System.out.println(loginDTO.toString() + "으로 로그인하자");
+	   
+	   try {
+		MemberVO loginMember = mService.login(loginDTO);
+		
+		if(loginMember != null) {
+			//System.out.println("MemberController - 로그인 성공");
+			
+			// 모델 객체에 loginMember를 바인딩 시킨다.
+			model.addAttribute("loginMember", loginMember);
+			
+			// 홈으로 이동
+			
+		} else {
+			//System.out.println("로그인 실패");
+		}
+	} catch (Exception e) {
+		
+		e.printStackTrace();
+	}
    }
 }
