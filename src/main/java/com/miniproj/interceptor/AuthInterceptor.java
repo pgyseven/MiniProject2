06d0d42c8512a26aa34c38665f7dab4da376d16f
gayoon-create <kgykgy1123@naver.com>
2024-08-08 11:04:44 +0900

    feat :  글에 대한 수정/삭제 권한이 있는지 검사하여 글수정/삭제 권한이 없다면 viewBoard로 이동하도록 처리함

--- src/main/java/com/miniproj/interceptor/AuthInterceptor.java
+++ src/main/java/com/miniproj/interceptor/AuthInterceptor.java
@@ -1,12 +1,19 @@
 package com.miniproj.interceptor;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import com.miniproj.model.BoardDetailInfo;
+import com.miniproj.model.MemberVO;
+import com.miniproj.service.hboard.HBoardService;
 import com.miniproj.util.DestinationPath;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * @작성자 : 802-02
@@ -22,13 +29,17 @@ import com.miniproj.util.DestinationPath;
     로그인이 되어있지 않아서 로그인 페이지로 끌려갔다면 로그인을 성공한 뒤에는 원래 하려던 기능의 페이지로 돌아가게 해야한다. /
     글수정, 글삭제, 댓글 수정/삭제 는 로그인되어 있어야할 뿐 아니라 그 글(댓글)의 주인인지도 확인해야 한다.
  */
+@RequiredArgsConstructor
 public class AuthInterceptor extends HandlerInterceptorAdapter {
+	
+	
+	   private final HBoardService service;
 
 	   /**
 	    * @작성자 : 802-02
 	    * @작성일 : 2024. 8. 7.
 	    * @메소드명 : preHandle
-	    * @parameter : request, response, handler
+	    * @parameter : HttpServletRequest request, HttpServletResponse response, Object handler
 	    * @throwsException : 
 	    * @description : 
 	    */
@@ -50,6 +61,26 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 	         
 	      } else { //로그인을 했다.
 	         System.out.println("[AuthInterceptor] : 로그인 OK 되어있다!");
+	         
+	         // 만약 글/댓글 수정, 글/댓글 삭제의 페이지에서 왔다면 그 글에 대한 수정/삭제 권한이 있는지?
+	         String uri = request.getRequestURI();
+	         if (uri.contains("modify") || uri.contains("remove")) {
+	        	 
+	        	 int boardNo = Integer.parseInt(request.getParameter("boardNo"));
+	        	 
+	        	 System.out.println(boardNo + "번 글에 대한 수정/삭제 권한이 있는지 검사하자");
+	        	 
+	        	 List<BoardDetailInfo> board = service.read(boardNo);
+	        	 
+	        	 // 로그인한 유저의 정보
+	        	 MemberVO loginMember = (MemberVO)ses.getAttribute("loginMember");
+	        	 
+	        	 if (!board.get(0).getWriter().equals(loginMember.getUserId())) {
+	        		 System.out.println("작성자와 로그인한 유저의 아이디가 달라서 쫓겨남");
+	        		 response.sendRedirect("/hboard/viewBoard?status=authFail&boardNo=" + boardNo);
+	        	 }
+	         }
+	         
 	         goOriginPath = true;
 	      }
 	      
