f0b80bec2f4e38f922307cd144604b0d3601edc6
gayoon-create <kgykgy1123@naver.com>
2024-08-20 10:10:17 +0900

    feat : 댓글 DTO 객체 생성

---
+++ src/main/java/com/miniproj/model/ReplyDTO.java
@@ -0,0 +1,21 @@
+package com.miniproj.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class ReplyDTO {
+	private String replyer;
+	private String content;
+	private int boardNo;
+}
